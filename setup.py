54af9c3d0b3e969b82ffe34922afdecaf8b04d66
zeffah <zeffah.eelly@gmail.com>
2024-02-02 09:21:21 +0300

    - Updated the app name in app config

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='dj-mpesa-payment',
-    version='0.0.1',
+    version='0.0.2',
     author='Elijah Onduso',
     description='Mpesa payment gateway',
     long_description=long_description,
